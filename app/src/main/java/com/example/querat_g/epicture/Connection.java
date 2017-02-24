efd37cb85e39d98f61419497d22dd0055dd9fff8
bellia_n <belliardnathan@gmail.com>
2017-02-24 16:29:03 +0100

    button manager connection view

--- app/src/main/java/com/example/querat_g/epicture/Connection.java
+++ app/src/main/java/com/example/querat_g/epicture/Connection.java
@@ -6,6 +6,7 @@ import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.widget.DrawerLayout;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -23,16 +24,44 @@ public class Connection extends MainActivity {
     private int       idImg;
     private Intent    intent;
     private EditText  txtUsername, txtPassword;
-    private Button    btnConnect;
+    private Button    btnConnect, btnCancel;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.setContentView(R.layout.connection);
+        loadClassVar();                                                                             // load class variables
+        loadDrawer();                                                                               // load left drawer
+        buttonManager();
+    }
+
+    /***
+     * init and set class variables
+     */
+    private void loadClassVar(){
         intent = getIntent();
         imgApi = (ImageView) findViewById(R.id.imageViewApi);
-        loadDrawer();
-        idImg = getResources().getIdentifier(intent.getStringExtra("api") + "_logo", "drawable", getPackageName());
-        imgApi.setImageResource(idImg);
+        idImg = getResources().getIdentifier(intent.getStringExtra("api") + "_logo",
+                "drawable", getPackageName());                                                      // get img res
+        txtPassword = (EditText) findViewById(R.id.editViewConnectPassword);                        // get password EditText
+        txtUsername = (EditText) findViewById(R.id.editViewConnectUsername);                        // get username EditText
+        btnConnect = (Button) findViewById(R.id.buttonConnectOk);                                   // get ok button
+        btnCancel = (Button) findViewById(R.id.buttonConnectCancel);                                // get cancel button
+        imgApi.setImageResource(idImg);                                                             // set img res
+    }
+
+    private void buttonManager(){
+        btnConnect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "ok button", Toast.LENGTH_LONG).show();
+            }
+        });
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "cancel button", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
